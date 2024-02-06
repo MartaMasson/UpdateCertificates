e5516c65987164422921aa7f209ee033b289b16e
Marta Guimarães <maguima@microsoft.com>
2024-02-06 15:46:35 -0300

    Getting data from queue message

--- UpdateCertificatesQueueTrigger.cs
+++ UpdateCertificatesQueueTrigger.cs
@@ -1,12 +1,11 @@
 using System;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Azure.Storage.Blobs;
 using Azure.Security.KeyVault.Certificates;
 using Azure.Identity;
-using System.IO;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace Company.Function
 {
@@ -17,54 +16,53 @@ namespace Company.Function
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
 
+            // Assuming the message content is a JSON string
+            var jsonContent = myQueueItem.ToString();
+
+            // Deserialize the JSON string into a custom object
+            var messageObject = JsonSerializer.Deserialize<MyCustomObject>(jsonContent);
+
+            // Extract values from the message
+            string blobName = messageObject.FileName;
+            string pfxPassword = messageObject.Pwd;
+            string certificateName = messageObject.certificateName;
+            log.LogInformation($"C# Queue trigger function - FileName {blobName} and Password {pfxPassword} and CertificateName {certificateName}...");
+
+            //Connecting to the blob storage
             string containerName = "selfsignedfiles"; // Replace with your container name
-            string blobName = "mypfx.pfx"; // Replace with your blob name
-            string pfxPassword = "@Teste123456"; // Replace with your PFX password
-
-
             var blobServiceClient = new BlobServiceClient(new Uri($"https://mmgcerts.blob.core.windows.net"), new DefaultAzureCredential());
             log.LogInformation($"C# Queue trigger function - Authenticating on Blob Storage using managed identity...");
 
+            // Retrieving the PFX file from the blob storage and transforming it into bytes
             var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
             var blobClient = containerClient.GetBlobClient(blobName);
             log.LogInformation($"C# Queue trigger function - Idenitified the pfx from the blob..." + blobClient.Name);
-
             byte[] pfxBytes = blobClient.DownloadContent().Value.Content.ToArray();
-
-            //var pfxContent = blobClient.OpenRead();
-            //log.LogInformation($"C# Queue trigger function - Got the file...");
-            //log.LogInformation($"File Content... Primeiro byte {pfxContent.ReadByte()} ");
-
-            // Convert the pfxContent stream to a byte array
-            /*byte[] pfxBytes;
-            using (var memoryStream = new MemoryStream())
-            {
-                pfxContent.CopyTo(memoryStream);
-                log.LogInformation($"C# memoryStream: {memoryStream}");
-                pfxBytes = memoryStream.ToArray();
-            }*/
             log.LogInformation($"C# Queue trigger function - File went to bytes...\n");
             log.LogInformation($"The file in bytes: {pfxBytes}");
 
-            // Create a CertificateClient to access the Key Vault
+            // Create a CertificateClient to connect and access the Key Vault
             var keyVaultUri = new Uri($"https://kv-vm-test-mmg.vault.azure.net/");
             var credential = new DefaultAzureCredential();
             var certificateClient = new CertificateClient(keyVaultUri, credential);
-
             log.LogInformation($"C# Queue trigger function - Connected into key vault...");
 
             // Import the PFX file into the Key Vault as a certificate
-            string certificateName = "mypfx"; // Replace with your certificate name
-
             var importOptions = new ImportCertificateOptions(certificateName, pfxBytes)
             {
-                Password = pfxPassword
+                Password = pfxPassword // Check if the password is really needed
             };
             await certificateClient.ImportCertificateAsync(importOptions);
 
             log.LogInformation($"C# Queue trigger function - Imported into key vault...");
-
         }
+    }
+
+    public class MyCustomObject
+    {
+        public string FileName { get; set; }
+        public string Pwd { get; set; }
+        public string certificateName { get; set; }
     }
 }
 
